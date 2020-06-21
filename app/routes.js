76a495795a28c9b8f124cb977c94f972df92a96c
Jon Maher <jonmaher@me.com>
2020-06-21 17:47:35 +0100

    uj3

--- app/routes.js
+++ app/routes.js
@@ -18,5 +18,50 @@ router.post('/uj2/nhs-login-answer', function (req, res) {
     }
 })
 
+router.post('/uj3/create-account-check', function (req, res) {
+
+    // Make a variable and give it the value from 'know-nhs-number'
+    var createaccount = req.session.data['createaccount']
+
+    // Check whether the variable matches a condition
+    if (createaccount == "True") {
+        // Send user to next page
+        res.redirect('https://svps-nhs-login.herokuapp.com/uj3/enter-email')
+    } else {
+        // Send user to ineligible page
+        res.redirect('/uj3/1')
+    }
+})
+
+
+router.post('/uj3/shield-check', function (req, res) {
+
+    // Make a variable and give it the value from 'know-nhs-number'
+    var shield = req.session.data['shield']
+
+    // Check whether the variable matches a condition
+    if (shield == "shield") {
+        // Send user to next page
+        res.redirect('/uj3/1')
+    } else {
+        // Send user to ineligible page
+        res.redirect('/uj3/88')
+    }
+})
+
+router.post('/uj3/conditions-check', function (req, res) {
+
+    // Make a variable and give it the value from 'know-nhs-number'
+    var conditions = req.session.data['medical_conditions']
+
+    // Check whether the variable matches a condition
+    if (conditions == "True") {
+        // Send user to next page
+        res.redirect('/uj3/1')
+    } else {
+        // Send user to ineligible page
+        res.redirect('/uj3/90')
+    }
+})
 
 module.exports = router
