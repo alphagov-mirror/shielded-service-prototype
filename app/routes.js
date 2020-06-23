fec665d82d67c64d4f58baa746e203eafa90ee21
Jon Maher <jonmaher@me.com>
2020-06-23 17:15:35 +0100

    e

--- app/routes.js
+++ app/routes.js
@@ -64,6 +64,21 @@ router.post('/uj3/create-account-check', function (req, res) {
     }
 })
 
+router.post('/uj3/create-account-check2', function (req, res) {
+
+    // Make a variable and give it the value from 'know-nhs-number'
+    var createaccount = req.session.data['createaccount']
+
+    // Check whether the variable matches a condition
+    if (createaccount == "False") {
+        // Send user to next page
+        res.redirect('/uj3/44')
+    } else {
+        // Send user to ineligible page
+        res.redirect('https://svps-nhs-login.herokuapp.com/uj3/enter-email')
+    }
+})
+
 
 router.post('/uj3/shield-check', function (req, res) {
 
